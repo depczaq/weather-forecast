199bf2d5927f0da281d7cf0a7ad8a0a69c9fc3ad
Miłosz Depczyński <depczak@gmail.com>
2018-12-02 21:47:02 +0100

    Forecast components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
+import { ForecastCardComponent } from 'app/components/weather/forecast-card/forecast-card.component';
 import { ForecastComponent } from 'app/components/weather/forecast/forecast.component';
+import { AuthorizationInterceptor } from 'app/interceptors/authorization-interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForecastComponent
+    ForecastComponent,
+    ForecastCardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizationInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/weather/forecast/forecast.component.ts
+++ src/app/components/weather/forecast/forecast.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Forecast } from 'app/models/forecast.model';
 import { WeatherService } from 'app/services/weather.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'wf-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.scss']
 })
-export class ForecastComponent implements OnInit {
+export class ForecastComponent implements OnInit, OnDestroy {
+
+  public forecast: Forecast;
+
+  private subscription: Subscription;
+
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
-    this.weatherService.getWeatherForecast('Gdańsk', 'PL').subscribe(
-      (response) => console.log(response)
-    );
-
+    this.subscription = this.weatherService.getWeatherForecast('Gdańsk', 'PL')
+      .subscribe(
+        next => this.forecast = next
+      );
   }
 
+  getCity() {
+    return this.forecast.city.name + ' (' + this.forecast.city.country + ')';
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
